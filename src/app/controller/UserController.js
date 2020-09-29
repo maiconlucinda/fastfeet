b82ad66ed6261b3ac5c4cad83c39bcc38786d215
Maicon Lucinda <mayconlucindatj60@gmail.com>
2020-09-29 06:20:53 -0300

    Schema Validation com Yup

--- src/app/controller/UserController.js
+++ src/app/controller/UserController.js
@@ -1,7 +1,18 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
@@ -18,6 +29,25 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required() : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email, oldPassword } = req.body;
 
     // Buscando no Banco de Dados informações já salvas do usuário.
